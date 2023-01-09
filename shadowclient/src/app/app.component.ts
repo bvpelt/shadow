341b3f05caf9ea69d3aad780f668e88abfb64a35
bart.vanpelt@gmail.com <bart.vanpelt@gmail.com>
2023-01-09 21:07:22 +0100

    First working version

--- shadowclient/src/app/app.component.ts
+++ shadowclient/src/app/app.component.ts
@@ -4,7 +4,7 @@ import { Component, ViewEncapsulation } from '@angular/core';
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
-  //encapsulation: ViewEncapsulation.ShadowDom
+  encapsulation: ViewEncapsulation.ShadowDom
 })
 export class AppComponent {
   title = 'Shawdowclient';
